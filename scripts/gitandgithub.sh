eff241fee97d9b13242a011c6989b75f614aa0ff
Samit-Singhvi <84736010+Samit-Singhvi@users.noreply.github.com>
2022-06-16 16:06:30 +0530

    Create gitandgithub.sh

---
+++ scripts/gitandgithub.sh
@@ -0,0 +1,26 @@
+#Installing Git
+sudo apt install git
+
+#Version Check
+sudo apt install git
+
+#Directory Accessible by Version Control
+git init
+
+#Addiing a file in git
+git add filename
+
+#Commit a file
+git commit -m "Message"
+
+#Cloning a local directory with github
+git clone "Link to the GitHub directory"
+
+#Pushing a local file to github
+git push
+
+#To check the status of files whether staged or not, committed or not
+git status
+
+#To view the commit history
+git log 
