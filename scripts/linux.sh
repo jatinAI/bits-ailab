ed871b5add0d9f2362c06118bb0f845e3f2e4d6f
Samit-Singhvi <84736010+Samit-Singhvi@users.noreply.github.com>
2022-06-16 15:58:33 +0530

    Create linux.sh

---
+++ scripts/linux.sh
@@ -0,0 +1,30 @@
+pwd
+ls
+ls -a
+ls -l
+cd
+mkdir
+touch filename
+rmdir
+mv
+cp
+find
+whoami
+hostname
+type
+type -a
+type -t
+type -p
+grep
+wc
+calc
+locate
+chown
+chmod
+
+#Hard Link
+    ln {source path] [new path]
+    
+#Soft Link
+    ln -s {source path] [new path]
+    
